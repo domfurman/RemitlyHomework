5087358b177cb75560489b1a7508fab9aece7ebb
Dominik Furman <s222915@student.uek.krakow.pl>
2024-04-22 18:32:38 +0200

    code rewritten into method

--- src/main/java/pl/remitly/Main.java
+++ src/main/java/pl/remitly/Main.java
@@ -5,22 +5,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
     static String pathname = "./src/main/java/pl/remitly/policy.json";
 
-    public static void main(String[] args) throws IOException {
+    public static boolean verification() throws IOException {
         try {
-//            File file = new File("./src/main/java/pl/remitly/policy.json");
-
             File file = new File(pathname);
             ObjectMapper objectMapper = new ObjectMapper();
             Policy policy = objectMapper.readValue(file, Policy.class);
             List<Policy.Statement> l = policy.getPolicyDocument().getStatement();
-            System.out.println(l.get(l.size()-1).getResource());
+            String resourceValue = l.get(l.size()-1).getResource();
+            if (Objects.equals(resourceValue, "*")) {
+                return false;
+            } else {
+                return true;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return true;
+    }
 
+    public static void main(String[] args) throws IOException {
+        System.out.println(Main.verification());
     }
 }
