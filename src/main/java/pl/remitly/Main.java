b19c5a170b61b5e1c9b4cde7d0dea0084768138d
Dominik Furman <s222915@student.uek.krakow.pl>
2024-04-22 19:22:51 +0200

    methods for finding and reading json file, unit test

--- src/main/java/pl/remitly/Main.java
+++ src/main/java/pl/remitly/Main.java
@@ -4,31 +4,43 @@ package pl.remitly;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 
 public class Main {
-    static String pathname = "./src/main/java/pl/remitly/policy.json";
+    static String fileToSearch = "policy.json";
+    static String fileNameRequired = "policy.json";
 
-    public static boolean verification() throws IOException {
+    public static File searchForFileInParentDirectory(String fileToFind) {
+        Path currentPath = Paths.get("").toAbsolutePath();
+        String path = currentPath.toString() + "/" + fileToFind;
+
+        File file = new File(path);
+
+        if (file.getName().equals(fileNameRequired)) {
+            return file;
+        } else {
+            return null;
+        }
+    }
+
+    public static Boolean verification(String fileToFind) throws IOException {
         try {
-            File file = new File(pathname);
+            File file = searchForFileInParentDirectory(fileToFind);
             ObjectMapper objectMapper = new ObjectMapper();
             Policy policy = objectMapper.readValue(file, Policy.class);
             List<Policy.Statement> l = policy.getPolicyDocument().getStatement();
             String resourceValue = l.get(l.size()-1).getResource();
-            if (Objects.equals(resourceValue, "*")) {
-                return false;
-            } else {
-                return true;
-            }
+            return !Objects.equals(resourceValue, "*");
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        return true;
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(Main.verification());
+        System.out.println(Main.verification(fileToSearch));
     }
 }
---
+++ src/test/java/pl/remitly/MainTest.java
@@ -0,0 +1,27 @@
+package pl.remitly;
+
+import org.junit.Test;
+import java.io.File;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    public void FileFindingTest() {
+
+        String fileToFind = "policy.json";
+
+        File result = Main.searchForFileInParentDirectory(fileToFind);
+
+        assertEquals(Main.fileNameRequired, result.getName());
+        assertNotNull(result, "File is empty");
+    }
+
+    @Test
+    public void verificationTest() {
+
+        String fileToFind = "policy.json";
+
+
+    }
+}
